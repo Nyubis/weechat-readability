7203b71a4a93aaf42286c3fc0637636956214128
Roel <nyubis@gmail.com>
2015-11-14 10:32:01 +0100

    Error checking and bugfix

--- readability.py
+++ readability.py
@@ -1,9 +1,7 @@
 import weechat as w
 
-w.register("readability", "nyubis", "0.0", "GPLv3",
-    "Make messages with markup more readable", "", "")
-
-w.prnt("asfd", "hello")
+w.register("readability", "nyubis", "0.1", "GPLv3",
+    "Strip colours from recent messages on demand", "", "")
 
 # dict of buffers to lists. Lists contain coloured messages from buffer
 storage = {}
@@ -13,10 +11,7 @@ w.hook_command("readable", "Show the last message with colours stripped out",
     "", "", "", "retrieve", "")
 
 def messagehook(data, buffer, date, tags, displayed, highlight, prefix, message):
-    w.prnt("asfd", "Received message: '%s'" % repr(message))
-    w.prnt("asfd", "len: %d" % len(message))
-    w.prnt("asfd", "buffer: %s" % buffer)
-    if not "\x19" in message:
+    if not "\x19" in message: #no colours in the message
         return w.WEECHAT_RC_OK
     if not buffer in storage:
         storage[buffer] = []
@@ -24,8 +19,15 @@ def messagehook(data, buffer, date, tags, displayed, highlight, prefix, message)
     return w.WEECHAT_RC_OK
 
 def retrieve(data, buffer, args):
-    index = -1
+    index = -1 #We need to index the list back to front, so negative indices
     if len(args) > 0:
-        index = -(args[0]+1)
+        index = -(int(args[0])+1)
+    if not buffer in storage:
+        w.prnt(buffer, "Error: No messages stored for this buffer")
+        return w.WEECHAT_RC_OK
+    if -index > len(storage[buffer]):
+        w.prnt(buffer, "Error: I only have %d messages for this buffer"
+            % len(storage[buffer]))
+        return w.WEECHAT_RC_OK
     w.prnt(buffer, w.string_remove_color(storage[buffer][index], ""))
     return w.WEECHAT_RC_OK
